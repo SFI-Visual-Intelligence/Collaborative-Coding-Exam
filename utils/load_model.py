40bb5c03612dbe60cebf0f00d9ebb8f51ef33146
salomaestro <chris10an.salomonsen@gmail.com>
2025-01-31 13:36:08 +0100

    Add ChristianModel: 2 layer CNN w/maxpooling

--- utils/load_model.py
+++ utils/load_model.py
@@ -1,12 +1,15 @@
 import torch.nn as nn
 
-from .models import MagnusModel
+from .models import ChristianModel, MagnusModel
 
 
-def load_model(modelname: str) -> nn.Module:
-    if modelname == "MagnusModel":
-        return MagnusModel()
-    else:
-        raise ValueError(
-            f"Model: {modelname} has not been implemented. \nCheck the documentation for implemented metrics, or check your spelling"
-        )
+def load_model(modelname: str, *args, **kwargs) -> nn.Module:
+    match modelname.lower():
+        case "magnusmodel":
+            return MagnusModel(*args, **kwargs)
+        case "christianmodel":
+            return ChristianModel(*args, **kwargs)
+        case _:
+            raise ValueError(
+                f"Model: {modelname} has not been implemented. \nCheck the documentation for implemented metrics, or check your spelling"
+            )
--- utils/models/__init__.py
+++ utils/models/__init__.py
@@ -1,3 +1,4 @@
-__all__ = ["MagnusModel"]
+__all__ = ["MagnusModel", "ChristianModel"]
 
+from .christian_model import ChristianModel
 from .magnus_model import MagnusModel
