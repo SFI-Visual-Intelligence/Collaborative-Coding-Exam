7c5a9b2707b11298e78635db41d3f8a6559b2f26
Solveig <solveig.thrun@web.de>
2025-02-01 11:26:56 +0100

    Added file for SolveigModel

--- utils/metrics/__init__.py
+++ utils/metrics/__init__.py
@@ -1,4 +1,5 @@
-__all__ = ["EntropyPrediction", "Recall"]
+__all__ = ["EntropyPrediction", "Recall", "F1"]
 
 from .EntropyPred import EntropyPrediction
 from .recall import Recall
+from .F1 import F1Score
